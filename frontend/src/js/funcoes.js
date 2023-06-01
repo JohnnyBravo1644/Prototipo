608e9109d742b643fce0e17681c757436db9269b
JohnnyBravo164 <joavitorjuv@outlook.com>
2023-06-01 00:31:39 -0300

    feat/Criadas duas funções para uma SideNav e exibir um ícone de carregando nas telas.

---
+++ frontend/src/js/funcoes.js
@@ -0,0 +1,17 @@
+//Carregando
+function esconderIconeCarregando() {
+    document.getElementById('loading').style.display = "none";
+  };
+  
+  function mostrarIconeCarregando(){
+    document.getElementById('loading').style.display = "block";
+  }
+  
+  //Nav-Bar
+  function openNav() {
+      document.getElementById("mySidenav").style.display = "block";
+    };
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.display = "none";
+    };
