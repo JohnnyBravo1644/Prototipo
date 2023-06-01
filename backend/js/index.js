feb809b33153b217911a7dad39a6a00982f0d6b3
JohnnyBravo164 <joavitorjuv@outlook.com>
2023-06-01 19:50:33 -0300

    feat/Criados os métodos do CRUD que iram interagir com a tabela de disciplina.

--- backend/js/index.js
+++ backend/js/index.js
@@ -18,12 +18,6 @@ const pool = require('./database');
     });
   });
   
-  app.get('/professor', (request, response) => {
-    pool.query('SELECT * FROM professor', (err, rows, fields) => {
-      response.send(rows);
-    });
-  });
-  
   app.get('/professores/:id', (request, response) => {
     const id = request.params.id;
   
@@ -93,6 +87,80 @@ const pool = require('./database');
   });
 
   //Disciplinas
+
+  app.get('/disciplinas', (request, response) => {
+    pool.query(`SELECT * FROM disciplinas`, (err, rows, fields) => {
+      return response.status(200).send(rows);
+    });
+  });
+
+  app.get('/disciplina/:id', (request, response) => {
+    const id = request.params.id;
+  
+    pool.query('SELECT * FROM disciplinas WHERE id = $1', [id], (err, result) => {
+      if (err) {
+        console.error('Erro ao executar a consulta:', err);
+        return response.status(500).send('Erro no servidor');
+      }
+  
+      const professor = result.rows[0];
+  
+      if (!professor) {
+        return response.status(404).send('Disciplina não encontrada');
+      }
+  
+      return response.status(200).send(professor);
+    });
+  });
+
+  app.post('/disciplina/inserir', (request, response) => {
+    const { nomeDisciplina, professorId, emailProfessor, periodo } = request.body;
+    
+    if (!nomeDisciplina || !professorId || !emailProfessor || !periodo) {
+      return response.status(400).send('Campos obrigatórios não foram fornecidos');
+    }
+    
+    pool.query('INSERT INTO disciplinas (nome, professor_id, professor_email, periodo) VALUES ($1, $2, $3, $4)', [nomeDisciplina, professorId, emailProfessor, periodo], (err, result) => {
+      if (err) {
+        console.error(err);
+        return response.status(500).send('Erro no servidor');
+      }
+      
+      response.status(201).send('Disciplina cadastrada com sucesso');
+    });
+  });
+
+  app.put('/disciplina/alterar/:id', (request, response) => {
+    const id = request.params.id;
+    if(! id) {
+      return response.status(400).send('Dados invalidos!');
+    }
+    const { nomeDisciplina, professorId, emailProfessor, periodo} = request.body;
+    
+    pool.query(`UPDATE disciplinas SET nome='${nomeDisciplina}', professor_id='${professorId}', professor_email='${emailProfessor}', periodo='${periodo}' WHERE id='${id}';`, (err, result) => {
+      if (err) {
+        console.error(err);
+        return response.status(500).send("Ocorreu um erro ao atualizar a disciplina");
+      }
+      
+      return response.status(200).send("Disciplina alterada com sucesso");
+    });
+  });
+
+  app.delete('/disciplina/deletar/:id', (request, response) => {
+    const id = request.params.id;
+    if(! id) {
+      return response.status(400).send('Dados invalidos!');
+    }
+    pool.query(`DELETE FROM disciplinas WHERE id = ${id}`, (err, result) => {
+      if (err) {
+        console.error('Erro ao executar a consulta:', err);
+        return response.status(500).send('Erro no servidor');
+      }
+  
+      return response.status(200).send("Disciplina deletada com sucesso");
+    });
+  });
 
   const PORT = 3002;
   app.listen(PORT, () => {
