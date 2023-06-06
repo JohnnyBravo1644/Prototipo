743f0423f18a461771db492c21889c39ddf5de6d
JohnnyBravo164 <joavitorjuv@outlook.com>
2023-06-06 00:23:15 -0300

    feat/Criada a função que exibe os professores.

--- frontend/src/js/disciplina-req.js
+++ frontend/src/js/disciplina-req.js
@@ -42,3 +42,39 @@ const selecionarProfessorDisciplina = () => {
 };
 
 selecionarProfessorDisciplina();
+
+const carregarDisciplinas = () => {
+    fetch('http://localhost:3002/disciplinas')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Erro ao obter os dados das disciplinas');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const rows = Array.isArray(data) ? data : [];
+            const dados = data.rows;
+
+            document.getElementById('disciplinas').innerHTML = dados.reverse().reduce((acumulador, disciplina) => {
+                console.log('Discplina', disciplina)
+                return acumulador + `
+                <tr>
+                <th "col-sm-3"><input class="formulario-alterar" type="text" id="disciplina-nome-${disciplina.codigoDisciplina}" value="${disciplina.nome}"></th>
+                <th "col-sm-3"><input class="formulario-alterar" type="text" id="disciplina-professor-${disciplina.codigoDisciplina}" value="${disciplina.nome}"></th>
+                <th class="col-sm-9"><input class="formulario-alterar" type="text" id="disciplina-email-${disciplina.codigoDisciplina}" value="${disciplina.professor_email}"></th>
+                <th class="col-sm-9"><input class="formulario-alterar" type="text" id="disciplina-dia-semana-${disciplina.codigoDisciplina}" value="${disciplina.dia_semana}"></th>
+                <th class="col-sm-9"><input class="formulario-alterar" type="text" id="disciplina-periodo-${disciplina.codigoDisciplina}" value="${disciplina.periodo}"></th>
+                <th class="col-sm-9"><button onclick="excluirDiscplina(${disciplina.codigoDisciplina})" id="deletar" class="btn btn-success">Deletar</button></th>
+                <th class="col-sm-9"><button onclick="alterarDiscplinas(${disciplina.codigoDisciplina})" id="atualizar" class="btn btn-success">Atualizar</button></th>
+            </tr>
+                `;
+            },
+             '', );
+            esconderIconeCarregando()
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
+carregarDisciplinas()
--- frontend/src/js/professor-req.js
+++ frontend/src/js/professor-req.js
@@ -10,18 +10,20 @@ const carregarProfesores = () => {
             const rows = Array.isArray(data) ? data : [];
             const dados = data.rows;
 
-            document.getElementById('professores').innerHTML = dados.reverse().reduce((acumulador, professor) => {
-                console.log('Professor', professor)
+            document.getElementById('disciplinas').innerHTML = dados.reverse().reduce((acumulador, disciplina) => {
+                console.log('Discplina', disciplina)
                 return acumulador + `
                 <tr>
-                <th "col-sm-3""><input class="formulario-alterar" type="text" id="professor-nome-${professor.id}"  value="${professor.nome}"></th>
-                <th "col-sm-3"><input class="formulario-alterar" type="text" id="professor-formacao-${professor.id}"  value="${professor.formacao}"></th>
-                <th class="col-sm-9"><input class="formulario-alterar" type="text" id="professor-email-${professor.id}"  value="${professor.email}"></th>
-                <th class="col-sm-9"><button onclick="excluirProfessor(${professor.id})" class="btn btn-success">Deletar</button></th>
-                <th class="col-sm-9"><button onclick="alterarProfessor(${professor.id})" class="btn btn-success">Atualizar</button></th>
+                <th "col-sm-3"><input class="formulario-alterar" type="text" id="disciplina-nome-${disciplina.id}" value="${disciplina.nomeDaDisciplina}"></th>
+                <th "col-sm-3"><input class="formulario-alterar" type="text" id="disciplina-professor-${disciplina.id}" value="${disciplina.nomeDoProfessor}"></th>
+                <th class="col-sm-9"><input class="formulario-alterar" type="text" id="disciplina-email-${disciplina.id}" value="${disciplina.email}"></th>
+                <th class="col-sm-9"><input class="formulario-alterar" type="text" id="disciplina-periodo-${disciplina.id}" value="${disciplina.periodo}"></th>
+                <th class="col-sm-9"><button onclick="excluirDiscplina(${disciplina.id})" id="deletar" class="btn btn-success">Deletar</button></th>
+                <th class="col-sm-9"><button onclick="alterarDiscplinas(${disciplina.id})" id="atualizar" class="btn btn-success">Atualizar</button></th>
             </tr>
                 `;
-            }, '');
+            },
+             '', );
             esconderIconeCarregando()
         })
         .catch((error) => {
