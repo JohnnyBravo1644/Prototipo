aa4e2a9ac09753a96a5242a6ef09bea1f12c2b44
JohnnyBravo164 <joavitorjuv@outlook.com>
2023-06-12 20:44:18 -0300

    feat/Criados novos arquivos especificos para exibição das tabelas.

---
+++ frontend/src/js/listar-horarios.js
@@ -0,0 +1,35 @@
+const carregarHorario = () => {
+    fetch('http://localhost:3002/horarios')
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Erro ao obter os dados das disciplinas');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const dados = data.rows;
+            console.log(data)
+            
+            document.getElementById('disciplinas').innerHTML = data.reverse().reduce((acumulador, horario) => {
+                console.log('Discplina', horario)
+                return acumulador + `
+                <tr>
+                <th "col-sm-3"><input class="formulario-alterar" type="text" id="disciplina-nome-${horario.id}" value="${horario.disciplina.nome_disciplina}"></th>
+                <th class="col-sm-9">${horario.professor.nome}</th>
+                <th class="col-sm-9">${horario.professor.email}</th>
+                <th class="col-sm-9">${horario.sala.bloco_sala} - ${horario.sala.numero_sala}</th>
+                <th class="col-sm-9"><input class="formulario-alterar" type="text" id="disciplina-dia-semana-${horario.id}" value="${horario.dia_semana}"></th>
+                <th class="col-sm-9"><input class="formulario-alterar" type="text" id="disciplina-periodo-${horario.id}" value="${horario.periodo}"></th>
+                <th class="col-sm-9"><button onclick="excluirHorario(${horario.id})" id="deletar" class="btn btn-success">Deletar</button></th>
+                <th class="col-sm-9"><button onclick="alterarHorario(${horario.id})" id="atualizar" class="btn btn-success">Atualizar</button></th>
+                </tr>
+                `;
+            },
+            '', );
+            esconderIconeCarregando()
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+    carregarHorario()
