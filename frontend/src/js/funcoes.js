ed61167e26d953706d087ea4e78ba0a7310ea0e3
JohnnyBravo164 <joavitorjuv@outlook.com>
2023-12-03 18:01:47 -0300

    feat/alteracao na estilisacao e posicionamento da sidenav

--- frontend/src/js/funcoes.js
+++ frontend/src/js/funcoes.js
@@ -9,12 +9,16 @@ function esconderIconeCarregando() {
   
   //Nav-Bar
   function openNav() {
-      document.getElementById("mySidenav").style.display = "block";
-    };
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
     
   function closeNav() {
-    document.getElementById("mySidenav").style.display = "none";
-  };
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
 
   const limparCampos = () => {
     document.getElementById('nome').value = '';
